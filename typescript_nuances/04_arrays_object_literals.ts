0c81341800908bfe7e1d7f49b09118e671fd596a
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-02-18 16:19:32 +0800

    object literal is a compact way of defining object
    object literal is a compact way of defining object just by using key-value pairs structure

---
+++ typescript_nuances/04_arrays_object_literals.ts
@@ -0,0 +1,19 @@
+/* Arrays */
+let targaryens : string[] = ["Aegon", "Rhaneyra", "Daemon", "Aemond"]
+
+// push a new item to the array
+targaryens.push("Daenerys")
+
+console.log(targaryens)
+
+
+/* Object Literals */
+let westerosi : { firstName:string, lastName:string, age:number } = {
+    firstName : "Aegon",
+    lastName : "Targaryen",
+    age: 25
+}
+
+westerosi.firstName = "Daenerys"
+
+console.log(westerosi)
