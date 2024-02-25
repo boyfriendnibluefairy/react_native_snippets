0a246c52017228c97151539444a43284571ff666
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-02-25 15:29:11 +0800

    tuple - predefined typed array

---
+++ typescript_nuances/08_named_tuples.ts
@@ -0,0 +1,21 @@
+/* Tuple - a typed array with predefined size */
+
+// let dragon: [boolean, string, number]
+
+// let isAlive:boolean = true
+// let firstName:string = "Sunfyre"
+// let age:number = 100
+
+// dragon = [isAlive, firstName, age]
+
+// console.log(dragon)
+
+
+/* Named Tuples */
+
+let castle : [firstName:string, xCoordinates:number, yCoordinates:number]
+
+castle = ["Winterfell", 166, 399]
+
+console.log(castle[2])
+console.log(castle)
