e4156cb2023c74b0b07aeefd0004b3da7e2a311b
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-04-11 06:54:38 +0800

    How to sort records

---
+++ mysql/17_READ_TABLE__ORDER_BY.sql
@@ -0,0 +1,11 @@
+USE mysql_westeros;
+
+SELECT * FROM westerosi
+ORDER BY first_name;
+
+SELECT * FROM westerosi
+ORDER BY last_name DESC;
+
+SELECT * FROM westerosi
+ORDER BY last_name ASC, first_name DESC;
+
