0522faed82385fb706d14bba842befcdbc3d96af
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-04-25 13:08:17 +0800

    How to enter php mode

---
+++ php/01_enter_php_mode.php
@@ -0,0 +1,11 @@
+// The tag below means you are entering the php mode.
+<?php ?>
+
+// A single php file can have multiple php tags.
+// Php tag can be written inside an html tag.
+<?php ?>
+    <h1> <?php echo "php inside html tag"; ?> </h1>
+<?php ?>
+
+// html tags cannot be put inside php tags.
+
