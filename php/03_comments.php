1595a5f16833468fc30140ef5983a0d213f52a1a
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-04-27 10:08:23 +0800

    Comments must be in php mode.

---
+++ php/03_comments.php
@@ -0,0 +1,14 @@
+<!-- filename: 03_comments.php -->
+
+<?php
+   // This is a single-line comment.
+   // It must be inside php mode.
+   echo "<h1> How to write comments. </h1>";
+
+   /*
+   This is a multi-line comment.
+   Comments must be inside the php mode.
+    */
+   echo "test echo";
+   # This is also a single-line comment.
+?>
