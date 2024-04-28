2a6bd9d18ce4ab440266950e3fcdd922163f4ae1
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-04-28 10:24:13 +0800

    var_dump() display info about variables

---
+++ php/04_variables.php
@@ -0,0 +1,14 @@
+<!-- filename: 04_variables.php -->
+
+<?php
+    /* Variables must begin with
+    dollar sign symbol.
+    */
+    $userAge = 33;
+    echo $userAge;
+    echo "</br>"; // insert new line
+
+    /* var_dump() is a built-in function
+    similar to echo that displays info about variable */
+    var_dump($userAge);
+?>
