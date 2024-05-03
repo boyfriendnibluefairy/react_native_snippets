06a8d27738187b949f1db819adb42f163849edee
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-05-03 08:55:44 +0800

    Perform arithmetic operations.

---
+++ php/09_arithmetic_operators.php
@@ -0,0 +1,16 @@
+<!-- filename: 09_arithmetic_operators.php -->
+
+<?php
+    // ARITHMETIC OPERATORS
+    echo "</br></br></br>";
+    $x = 6;
+    $y = 3;
+    //echo (+$x);
+    // echo (-$x);
+    // echo ($x + $y);
+    // echo ($x - $y);
+    // echo ($x * $y);
+    // echo ($x / $y);
+    // echo ($x % $y);
+    echo ($x ** $y);
+?>
