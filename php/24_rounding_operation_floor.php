33e4d4a44e5e9d4b9536a9ecd904dcb6c0fc193f
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-05-20 11:19:52 +0800

    floor() rounds off to lower integer

---
+++ php/24_rounding_operation_floor.php
@@ -0,0 +1,18 @@
+<!-- filename: 24_rounding_operation_floor.php -->
+
+<?php
+    $x = 3.141593;
+    echo $x;
+    echo "<br/><br/>";
+
+    echo floor($x);
+    echo "<br/>";
+    echo floor(-3.141593);
+    echo "<br/><br/>";
+
+    echo floor(1.1);
+    echo "<br/>";
+    echo floor(1.9);
+    echo "<br/><br/>";
+
+?>
