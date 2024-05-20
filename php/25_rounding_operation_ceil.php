ba5fedbbc40a504d0fb34f0a08e86e305af9e55b
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-05-21 07:02:28 +0800

    ceil() rounds up to the next higher integer

---
+++ php/25_rounding_operation_ceil.php
@@ -0,0 +1,18 @@
+<!-- filename: 24_rounding_operation_ceil.php -->
+
+<?php
+    $x = 3.141593;
+    echo $x;
+    echo "<br/><br/>";
+
+    echo ceil($x);
+    echo "<br/>";
+    echo ceil(-3.141593);
+    echo "<br/><br/>";
+
+    echo ceil(9.1);
+    echo "<br/>";
+    echo ceil(9.9);
+    echo "<br/><br/>";
+
+?>
