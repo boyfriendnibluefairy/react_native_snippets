a337c641e11ba0262fe5ba1e0b7981852a597257
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-05-22 10:51:23 +0800

    round() controls decimal places

---
+++ php/26_rounding_operation.php
@@ -0,0 +1,11 @@
+<!-- filename: 26_rounding_operation.php -->
+
+<?php
+    $x = 3.141593;
+    echo $x;
+    echo "<br/><br/>";
+
+    echo round($x);
+    echo "<br/>";
+    echo round($x, 3);
+?>
